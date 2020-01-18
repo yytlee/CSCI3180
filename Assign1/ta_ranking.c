70922c97f5e1b6df50eaef2d8b081970d103b46c
yytlee <yytlee2712@gmail.com>
2020-01-18 22:27:18 +0800

    read instructors.txt

--- Assign1/ta_ranking.c
+++ Assign1/ta_ranking.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct instructor{
     int courseID;
@@ -13,16 +14,51 @@ typedef struct candidate{
     int pref[4];
 }CANDIDATE;
 
-int main(void){
+char* substr(char *src, int start, int end)
+{
+	int len = end - start + 1;
+	char *str = (char*)malloc(sizeof(char) * len);
+	for(int i = start; (i < end) && (*src != '\0'); i++)
+	{
+		*str = *(src + i);
+		str++;
+	}
+	*str = '\0';
+	return str - len + 1;
+}
 
-    FILE *instr_fp = fopen("/testcase/instructors.txt", "r");
+int main(void){
+    int num_instr = 0;
+    INSTRUCTOR *instr = (INSTRUCTOR*)malloc(sizeof(INSTRUCTOR*));
+    char tmp[125];
+
+    FILE *instr_fp = fopen("./testcase/instructors.txt", "r");
     if(instr_fp == NULL)printf("error");
 
-    s
+    while(fgets(tmp, 125, instr_fp) != NULL){
+        printf("%s\n", tmp);
+        num_instr++;
+        instr = (INSTRUCTOR*)realloc(instr, sizeof(INSTRUCTOR) * num_instr);
+        sscanf(tmp, "%d", &instr[num_instr - 1].courseID);
+        int pt = 5;
+        for(int i = 0; i < 3; i++){
+            instr[num_instr - 1].req_skill[i] = substr(tmp, pt, pt + 15);
+            pt += 15;
+            printf("-> %s\n", instr[num_instr - 1].req_skill[i]);
+        }
+        for(int i = 0; i < 5; i++){
+            instr[num_instr - 1].opt_skill[i] = substr(tmp, pt, pt + 15);
+            pt += 15;
+            printf("-> %s\n", instr[num_instr - 1].opt_skill[i]);
+        }
 
-    FILE *candi_fp = fopen("/testcase/candidates.txt", "r");
+        fgets(tmp, 125, instr_fp);
+    }
+
+    FILE *candi_fp = fopen("./testcase/candidates.txt", "r");
     if(candi_fp == NULL)printf("error");
 
     fclose(instr_fp);
+    fclose(candi_fp);
     return 0;
 }
