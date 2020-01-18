ffcb4bfe6783a482add402107e55c1630824ac3b
yytlee <yytlee2712@gmail.com>
2020-01-18 23:01:47 +0800

    read candidates.txt

--- Assign1/ta_ranking.c
+++ Assign1/ta_ranking.c
@@ -29,14 +29,16 @@ char* substr(char *src, int start, int end)
 
 int main(void){
     int num_instr = 0;
+    int num_candi = 0;
     INSTRUCTOR *instr = (INSTRUCTOR*)malloc(sizeof(INSTRUCTOR*));
-    char tmp[125];
+    CANDIDATE *candi = (CANDIDATE*)malloc(sizeof(CANDIDATE*));
+    char tmp[147];
 
     FILE *instr_fp = fopen("./testcase/instructors.txt", "r");
     if(instr_fp == NULL)printf("error");
 
     while(fgets(tmp, 125, instr_fp) != NULL){
-        printf("%s\n", tmp);
+        //printf("%s\n", tmp);
         num_instr++;
         instr = (INSTRUCTOR*)realloc(instr, sizeof(INSTRUCTOR) * num_instr);
         sscanf(tmp, "%d", &instr[num_instr - 1].courseID);
@@ -44,21 +46,37 @@ int main(void){
         for(int i = 0; i < 3; i++){
             instr[num_instr - 1].req_skill[i] = substr(tmp, pt, pt + 15);
             pt += 15;
-            printf("-> %s\n", instr[num_instr - 1].req_skill[i]);
+            //printf("-> %s\n", instr[num_instr - 1].req_skill[i]);
         }
         for(int i = 0; i < 5; i++){
             instr[num_instr - 1].opt_skill[i] = substr(tmp, pt, pt + 15);
             pt += 15;
-            printf("-> %s\n", instr[num_instr - 1].opt_skill[i]);
+            //printf("-> %s\n", instr[num_instr - 1].opt_skill[i]);
         }
 
         fgets(tmp, 125, instr_fp);
     }
+    fclose(instr_fp);
 
     FILE *candi_fp = fopen("./testcase/candidates.txt", "r");
     if(candi_fp == NULL)printf("error");
 
-    fclose(instr_fp);
+    while(fgets(tmp, 146, candi_fp) != NULL){
+        //printf("%s\n", tmp);
+        num_candi++;
+        candi = (CANDIDATE*)realloc(candi, sizeof(CANDIDATE) * num_candi);
+        sscanf(tmp, "%d", &candi[num_candi - 1].ID);
+        int pt = 11;
+        for(int i = 0; i < 8; i++){
+            candi[num_candi - 1].skill[i] = substr(tmp, pt, pt + 15);
+            pt += 15;
+            //printf("-> %s\n", candi[num_candi - 1].skill[i]);
+        }
+        sscanf(substr(tmp, pt, pt + 15), "%d %d %d", &candi[num_candi - 1].pref[0], &candi[num_candi - 1].pref[1], &candi[num_candi - 1].pref[2]);
+        //printf("%d %d %d\n", candi[num_candi - 1].pref[0], candi[num_candi - 1].pref[1], candi[num_candi - 1].pref[2]);
+        fgets(tmp, 146, instr_fp);
+    }
     fclose(candi_fp);
+
     return 0;
 }
