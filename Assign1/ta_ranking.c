d56bf0c477cc9a2bc841c2f4001544fd2baca127
yytlee <yytlee2712@gmail.com>
2020-02-08 16:08:03 +0800

    modify to adope compiler

--- Assign1/ta_ranking.c
+++ Assign1/ta_ranking.c
@@ -35,7 +35,8 @@ typedef struct candidate{
 char* substr(char *src, int start, int end){
 	int len = end - start + 1;
 	char *str = (char*)malloc(sizeof(char) * len);
-	for(int i = start; (i < end) && (*src != '\0'); i++)
+    int i;
+	for(i = start; (i < end) && (*src != '\0'); i++)
 	{
 		*str = *(src + i);
 		str++;
@@ -45,8 +46,9 @@ char* substr(char *src, int start, int end){
 }
 
 int req_satisf(INSTRUCTOR in, CANDIDATE ca){
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 8; j++){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 8; j++){
             if(strcmp(in.req_skill[i], ca.skill[j]) == 0) break;
             if(j == 7) return 0;
         }
@@ -57,8 +59,9 @@ int req_satisf(INSTRUCTOR in, CANDIDATE ca){
 double score(INSTRUCTOR in, CANDIDATE ca){
     if(req_satisf(in, ca) == 0) return 0;
     double s = 1;
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 8; j++){
+    int i, j;
+    for(i = 0; i < 5; i++){
+        for(j = 0; j < 8; j++){
             if(strcmp(in.opt_skill[i], ca.skill[j]) == 0){
                 s++;
                 break;
@@ -73,9 +76,10 @@ double score(INSTRUCTOR in, CANDIDATE ca){
 }
 
 void print_score(INSTRUCTOR *instr, CANDIDATE *candi, int in, int ca){
-    for(int i = 0; i < in; i++){
+    int i, j;
+    for(i = 0; i < in; i++){
         printf("%d: ", instr[i].courseID);
-        for(int j = 0; j < ca; j++){
+        for(j = 0; j < ca; j++){
             printf("%.1f ", score(instr[i], candi[j]));
         }
         printf("\n");
@@ -88,6 +92,7 @@ int main(void){
     INSTRUCTOR *instr = (INSTRUCTOR*)malloc(sizeof(INSTRUCTOR));
     CANDIDATE *candi = (CANDIDATE*)malloc(sizeof(CANDIDATE));
     char tmp[147];
+    int i, j, k;
 
     FILE *instr_fp = fopen("./testcase/instructors.txt", "r");
     if(instr_fp == NULL){
@@ -101,12 +106,12 @@ int main(void){
         instr = (INSTRUCTOR*)realloc(instr, sizeof(INSTRUCTOR) * num_instr);
         sscanf(tmp, "%d", &instr[num_instr - 1].courseID);
         int pt = 5;
-        for(int i = 0; i < 3; i++){
+        for(i = 0; i < 3; i++){
             instr[num_instr - 1].req_skill[i] = substr(tmp, pt, pt + 15);
             pt += 15;
             //printf("-> %s\n", instr[num_instr - 1].req_skill[i]);
         }
-        for(int i = 0; i < 5; i++){
+        for(i = 0; i < 5; i++){
             instr[num_instr - 1].opt_skill[i] = substr(tmp, pt, pt + 15);
             pt += 15;
             //printf("-> %s\n", instr[num_instr - 1].opt_skill[i]);
@@ -128,7 +133,7 @@ int main(void){
         candi = (CANDIDATE*)realloc(candi, sizeof(CANDIDATE) * num_candi);
         sscanf(tmp, "%d", &candi[num_candi - 1].ID);
         int pt = 11;
-        for(int i = 0; i < 8; i++){
+        for(i = 0; i < 8; i++){
             candi[num_candi - 1].skill[i] = substr(tmp, pt, pt + 15);
             pt += 15;
             //printf("-> %s\n", candi[num_candi - 1].skill[i]);
@@ -145,10 +150,10 @@ int main(void){
         exit(-1);
     }
 
-    for(int i = 0; i < num_instr; i++){
+    for(i = 0; i < num_instr; i++){
         int suit_candi = 0;
         int rank[3][2] = {{0}};
-        for(int j = 0; j < num_candi; j++){
+        for(j = 0; j < num_candi; j++){
             int s = score(instr[i], candi[j]);
             if(s > rank[0][1] || (s == rank[0][1] && candi[j].ID < rank[0][0])){
                 suit_candi++;
@@ -173,7 +178,7 @@ int main(void){
             }
         }
         fprintf(output_fp, "%d ", instr[i].courseID);
-        for(int k = 0; k < 3; k++){
+        for(k = 0; k < 3; k++){
             if(rank[k][0] == 0 && rank[k][1] == 0){
                 fputs("0000000000 ", output_fp);
             }
