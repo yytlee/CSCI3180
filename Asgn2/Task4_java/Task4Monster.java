baac219ad56620fd255cd78596c47acaa7b50d03
yytlee <yytlee2712@gmail.com>
2020-03-12 13:00:53 +0800

    add header

--- Asgn2/Task4_java/Task4Monster.java
+++ Asgn2/Task4_java/Task4Monster.java
@@ -1,3 +1,20 @@
+/*
+ * CSCI3180 Principles of Programming Languages
+ *
+ * --- Declaration ---
+ *
+ * I declare that the assignment here submitted is original except for source
+ * material explicitly acknowledged. I also acknowledge that I am aware of
+ * University policy and regulations on honesty in academic work, and of the
+ * disciplinary guidelines and procedures applicable to breaches of such policy
+ * and regulations, as contained in the website
+ * http://www.cuhk.edu.hk/policy/academichonesty/
+ *
+ * Assignment 2
+ * Name : Lee Tsz Yan
+ * Student ID : 1155110177
+ * Email Addr : tylee8@cse.cuhk.edu.hk
+ */
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
