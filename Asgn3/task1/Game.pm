45f29c8fcbcd3d87fa2d23425bc8cfed7d5e5a43
yytlee <yytlee2712@gmail.com>
2020-04-10 16:31:10 +0800

    add asgn3

---
+++ Asgn3/task1/Game.pm
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+
+package Game;
+use MannerDeckStudent; 
+use Player;
+
+sub new {
+	
+}
+
+sub setPlayers {
+
+}
+
+sub getReturn {
+
+}
+
+sub showCards {
+	
+}
+
+sub startGame {
+
+}
+
+return 1;
