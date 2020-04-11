c595ddab5442d314d598f61e26465dd52fa823dc
yytlee <yytlee2712@gmail.com>
2020-04-12 02:34:46 +0800

    task1 player

--- Asgn3/task1/Player.pm
+++ Asgn3/task1/Player.pm
@@ -3,15 +3,32 @@ use warnings;
  
 package Player;
 sub new {
+    my $class = shift @_;
+    my @cards = ();
+    my $self = {
+        _name => shift @_,
+        _cards => \@cards,
+    };
+    my $object = bless $self, $class;
+    return $object;
 }
 
 sub getCards {
+    my $self = @_;
+    my $card = @_;
+    push(@{$self->{_name}}, $card);
 }
 
 sub dealCards {
+    my $self = @_;
+    my $card = shift(@{$self->{_cards}});
+    return $card;
 }
 
 sub numCards {
+    my $self = @_;
+    my $size = $self->{_cards};
+    return $size;
 }
 
 return 1;
