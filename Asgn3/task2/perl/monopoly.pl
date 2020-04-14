38406a8d03cc010d7ac4cbb0788192fd023ee8ba
yytlee <yytlee2712@gmail.com>
2020-04-15 01:28:29 +0800

    add header

--- Asgn3/task2/perl/monopoly.pl
+++ Asgn3/task2/perl/monopoly.pl
@@ -1,3 +1,21 @@
+# /*
+#  * CSCI3180 Principles of Programming Languages
+#  *
+#  * --- Declaration ---
+#  *
+#  * I declare that the assignment here submitted is original except for source
+#  * material explicitly acknowledged. I also acknowledge that I am aware of
+#  * University policy and regulations on honesty in academic work, and of the
+#  * disciplinary guidelines and procedures applicable to breaches of such policy
+#  * and regulations, as contained in the website
+#  * http://www.cuhk.edu.hk/policy/academichonesty/
+#  *
+#  * Assignment 3
+#  * Name : Lee Tsz Yan
+#  * Student ID : 1155110177
+#  * Email Addr : tylee8@cse.cuhk.edu.hk
+#  */
+
 #! /usr/bin/perl
 use warnings;
 use strict;
